decb26dfb49226e9448dc7006da00bea089c5097
benlopes <rubenssouss@hotmail.com>
2021-10-25 17:02:39 -0300

    feat: add the listing products service function

--- services/productsService.js
+++ services/productsService.js
@@ -14,6 +14,26 @@ const add = async (productData) => {
   return { status: 201, data: addedProduct };
 };
 
+const getAll = async () => {
+  const products = await product.getAll();
+
+  return { status: 200, data: products };
+};
+
+const getById = async (id) => {
+  const returnedProduct = await product.getById(id);
+
+  const message = 'Wrong id format';
+
+  if (!returnedProduct) {
+    return { status: 422, message };
+  }
+
+  return { status: 200, data: returnedProduct };
+};
+
 module.exports = {
   add,
+  getAll,
+  getById,
 };
