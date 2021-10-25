f063fee781485b030d73bcf1692518133684188d
benlopes <rubenssouss@hotmail.com>
2021-10-25 17:21:56 -0300

    add express app

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser').json();
+
+const app = express();
+app.use(bodyParser);
+
+const PORT = '3000';
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.listen(PORT, () => {
+  console.log('Online');
 });
