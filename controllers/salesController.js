c25a17da6336237c3ec35c0afbf0df5347a4e8c9
benlopes <rubenssouss@hotmail.com>
2021-10-25 20:52:27 -0300

    feat: add the sales listing controller functions

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -16,6 +16,26 @@ const add = async (req, res) => {
   res.status(status).json(data);
 };
 
+const getAll = async (_req, res) => {
+  const { status, data } = await salesService.getAll();
+
+  res.status(status).json({ sales: data });
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const { status, data, message } = await salesService.getById(id);
+
+  if (message) {
+    return res.status(status)
+      .json({ err: { code: 'not_found', message } });
+  }
+  
+  res.status(status).json(data);
+};
+
 module.exports = {
   add,
+  getAll,
+  getById,
 };
