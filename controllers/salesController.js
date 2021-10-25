c9a112ba6b1e8b96b2e864b5f94e82a4fa13992b
benlopes <rubenssouss@hotmail.com>
2021-10-25 20:56:58 -0300

    feat: add the update sales controller function

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -34,8 +34,17 @@ const getById = async (req, res) => {
   res.status(status).json(data);
 };
 
+const update = async (req, res) => {
+  const { id } = req.params;
+
+  const { status, data } = await salesService.update(id, req.body);
+
+  res.status(status).json(data);
+};
+
 module.exports = {
   add,
   getAll,
   getById,
+  update,
 };
